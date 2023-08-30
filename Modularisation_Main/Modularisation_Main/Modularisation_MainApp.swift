6c984c7936cc0830518e3d6e4e9174aaf4a86cf7
Paulo H.M <paulo.machtura@vee.digital>
2023-08-29 22:53:19 -0300

    basic structure

---
+++ Modularisation_Main/Modularisation_Main/Modularisation_MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modularisation_MainApp.swift
+//  Modularisation_Main
+//
+//  Created by Paulo H.M. on 29/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Modularisation_MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
